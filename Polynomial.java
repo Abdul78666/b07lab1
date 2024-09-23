26589da74e48a2ac7fc38f78ce99ebc52c14798b
Abdul78666 <121519218+Abdul78666@users.noreply.github.com>
2024-09-22 21:11:53 -0400

    Update Polynomial.java

--- Polynomial.java
+++ Polynomial.java
@@ -13,28 +13,26 @@ public Polynomial(double[] newarray) {
         array[i] = newarray[i];
     }
 }
-public Polynomial add(Polynomial other) {
-    int maxLength = Math.max(this.array.length, other.array.length);
-    
-     double[] result = new double[maxLength];
-    
-     for (int i = 0; i < maxLength; i++) {
-        double thisCoeff = 0;
-        double otherCoeff = 0;
+public Polynomial add(Polynomial newArray) {
+		int lengthArrayOne = this.array.length;
+		int lengthArrayTwo = newArray.array.length;
+ 	    int max = Math.max(this.array.length, newArray.array.length);
+ 	    double[] result = new double[max];
+	    
+ 	    for (int i = 0; i < Math.min(lengthArrayOne, lengthArrayTwo); i++) {
+	        result[i] = this.array[i] + newArray.array[i];
+	    }
 
-         if (i < this.array.length) {
-            thisCoeff = this.array[i];
-        }
+ 	    for (int i = lengthArrayTwo; i < lengthArrayOne; i++) {
+	        result[i] = this.array[i];
+	    }
 
-         if (i < other.array.length) {
-            otherCoeff = other.array[i];
-        }
-
-         result[i] = thisCoeff + otherCoeff;
-    }
-    
-     return new Polynomial(result);
-}		
+ 	    for (int i = lengthArrayOne; i < lengthArrayTwo; i++) {
+	        result[i] = newArray.array[i];
+	    }
+	    
+ 	    return new Polynomial(result);
+}
 public double evaluate(double x) {
     double result = 0;
     int y = 0;   
