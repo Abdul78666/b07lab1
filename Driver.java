a238c5d24451aebb464b140c1a5523c861c4d018
Abdul78666 <121519218+Abdul78666@users.noreply.github.com>
2024-09-29 22:51:06 -0400

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -1,19 +1,19 @@
 package PrePractice;
 
 public class Driver {
-	public static void main(String [] args) {
-		Polynomial p = new Polynomial();
-		System.out.println(p.evaluate(3));
-		double [] c1 = {6,0,0,5};
-		Polynomial p1 = new Polynomial(c1);
-		double [] c2 = {0,-2,0,0,-9};
-		Polynomial p2 = new Polynomial(c2);
-		Polynomial s = p1.add(p2);
-		System.out.println("s(0.1) = " + s.evaluate(0.1));
-		if(s.hasRoot(1))
-		System.out.println("1 is a root of s");
-		else
-		System.out.println("1 is not a root of s");
-	}
-	}
-
+    public static void main(String[] args) {
+        double[] a = {6, 0, 0, 5};
+        int[] b = {6, 2, 4, 5};
+        Polynomial i = new Polynomial(a, b);
+        System.out.println("Polynomial p1: " + i);
+        double[] x = {5,2, 7, 8};
+        int[] y = {3,7,544, 42};
+        Polynomial j = new Polynomial(x, y);
+        System.out.println("Polynomial p2: " + j);
+        Polynomial multiply = i.multiply(j);
+        System.out.println("Sum (p1 + p2): " + multiply);
+        Polynomial sum = i.add(j);
+        System.out.println("Sum (p1 + p2): " + sum);
+         System.out.println(i.hasRoot(0));
+    }
+}
